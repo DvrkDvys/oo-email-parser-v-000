7ccf3ef656c8d2c55f501b488e25b03ab90e58c1
Jordan Harris <jharris@wework.com>
2018-07-12 19:00:56 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,4 +10,13 @@ class EmailParser
     @@all
   end
   
-  
+  def self.parser(emails)
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
+
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
